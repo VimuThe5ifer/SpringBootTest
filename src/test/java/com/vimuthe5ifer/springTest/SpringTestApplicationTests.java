0fbb12afdccf413fbcca38e17e2dfe2a7d9537e2
Vimukthi Sampath Charles <vimuthe5ifer@gmail.com>
2024-11-04 05:29:09 +0530

    Initialized the project

---
+++ src/test/java/com/vimuthe5ifer/springTest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimuthe5ifer.springTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
